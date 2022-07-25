bc34281164f08e189e990a702277a7101bbc32cd
Andrew Laing <parisianconnections@gmail.com>
2022-07-25 12:10:57 +0100

    Added CollaborativeFiltering class for making recommendations

---
+++ CollaborativeFiltering/Recommendations.cs
@@ -0,0 +1,335 @@
+﻿using static System.Math;
+
+namespace CollaborativeFiltering
+{
+    public delegate double SimilarityScore(string category_1, string category_2);
+
+    public struct CategoryScore
+    {
+        public string name;
+        public double value;
+    }
+
+    public struct FeatureScore
+    {
+        public string name;
+        public double value;
+    }
+
+    public class Recommendations
+    {
+        private Dictionary<string, Dictionary<string, double>> dataset;     // category(feature: value, ...), category(feature ... 
+
+        public Recommendations()
+        {
+            dataset = new Dictionary<string, Dictionary<string, double>>();
+        }
+
+        #region "Dataset management functions"
+
+        public bool ContainsCategory(string category_name)
+        {
+            return dataset.ContainsKey(category_name);
+        }
+
+        public bool ContainsFeatureInCategory(string category_name, string feature_name)
+        {
+            return dataset.ContainsKey(category_name) && dataset[category_name].ContainsKey(feature_name);
+        }
+
+        public void AddCategory(string category_name)
+        {
+            if (!ContainsCategory(category_name))
+            {
+                dataset.Add(category_name, new Dictionary<string, double>());
+            }
+        }
+
+        public Dictionary<string, Dictionary<string, double>>.KeyCollection GetCategoryNames()
+        {
+            return dataset.Keys;
+        }
+
+        public Dictionary<string, double>.KeyCollection GetFeatureNamesInCategory(string category_name)
+        {
+            if (ContainsCategory(category_name))
+            {
+                return dataset[category_name].Keys;
+            }
+            else
+            {
+                return new Dictionary<string, double>().Keys;
+            }
+        }
+
+        public void AddValueForFeatureToCategory(string category_name, string feature_name, double rating)
+        {
+            AddCategory(category_name);
+
+            if (!dataset[category_name].ContainsKey(feature_name))
+            {
+                dataset[category_name].Add(feature_name, rating);
+            }
+            else
+            {
+                dataset[category_name][feature_name] = rating;
+            }
+        }
+
+        public double GetValueForFeatureInCategory(string category_name, string feature_name)
+        {
+            if (ContainsFeatureInCategory(category_name, feature_name))
+            {
+                return dataset[category_name][feature_name];
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+        #endregion
+
+        #region "Distance Scoring functions"
+        /** 
+         * Choosing a distance metric.
+         * 
+         * Euclidean - Observations with high/low values of features will be grouped together; Groupings are dependant on magnitude.
+         * Pearson   - Useful to correct for grade inflation.
+         * Tanimoto  - Useful when looking for similarity of feature occurence, or those woth binary values.
+         * 
+         *        e.g, A bought 1x eggs, 1x flour and 1x sugar.
+         *             B bought 100x eggs, 100x flour and 100x sugar.
+         *             C bought 1x eggs, 1x Vodka and 1x Red Bull.
+         *             Whilst using Euclidean will find that C is more similar to A, Pearson and Tanimoto show B and A as more similar.
+         */
+
+
+        public double EuclideanDistanceScore(string category_1, string category_2)
+        {
+            if (!ContainsCategory(category_1) || !ContainsCategory(category_2))
+            {
+                return 0.0;
+            }
+
+            double sum_of_squares = 0.0;
+            Dictionary<string, double>.KeyCollection critic_1_movies = GetFeatureNamesInCategory(category_1);
+            Dictionary<string, double>.KeyCollection critic_2_movies = GetFeatureNamesInCategory(category_2);
+
+            foreach (string key1 in critic_1_movies)
+            {
+                foreach (string key2 in critic_2_movies)
+                {
+                    if (key1 == key2)
+                    {
+                        double rating_c1 = GetValueForFeatureInCategory(category_1, key1);
+                        double rating_c2 = GetValueForFeatureInCategory(category_2, key2);
+                        sum_of_squares += Pow((rating_c1 - rating_c2), 2);
+                    }
+                }
+            }
+
+            // if they have no ratings in common
+            if (sum_of_squares == 0.0)
+            {
+                return 0.0;
+            }
+
+            return 1 / (1 + sum_of_squares);
+        }
+
+        public double PearsonCorrelationScore(string category_1, string category_2)
+        {
+            if (!ContainsCategory(category_1) || !ContainsCategory(category_2))
+            {
+                return 0.0;
+            }
+
+            double sum_c1 = 0.0;                // sum of mutual item ratings for critic_1 
+            double sum_c2 = 0.0;
+            double sum_of_squares_c1 = 0.0;     // sum of squares of mutual item ratings for critic_1 
+            double sum_of_squares_c2 = 0.0;
+            double sum_of_products = 0.0;       // sum of products of rating by critic_1 for item * those of critic_2 
+            int n = 0;                          // number of mutually rated items
+            Dictionary<string, double>.KeyCollection critic_1_movies = GetFeatureNamesInCategory(category_1);
+            Dictionary<string, double>.KeyCollection critic_2_movies = GetFeatureNamesInCategory(category_2);
+
+            foreach (string key1 in critic_1_movies)
+            {
+                foreach (string key2 in critic_2_movies)
+                {
+                    if (key1 == key2)
+                    {
+                        double rating_c1 = GetValueForFeatureInCategory(category_1, key1);
+                        double rating_c2 = GetValueForFeatureInCategory(category_2, key2);
+                        sum_c1 += rating_c1;
+                        sum_c2 += rating_c2;
+                        sum_of_squares_c1 += Pow(rating_c1, 2);
+                        sum_of_squares_c2 += Pow(rating_c2, 2);
+                        sum_of_products += (rating_c1 * rating_c2);
+                        n++;
+                    }
+                }
+            }
+
+            // if they have no ratings in common
+            if (n == 0)
+            {
+                return 0.0;
+            }
+
+            // Calculate PearsonScore
+            double numerator = sum_of_products - (sum_c1 * sum_c2 / n);
+            double denominator = Sqrt((sum_of_squares_c1 - Pow(sum_c1, 2) / n) * (sum_of_squares_c2 - Pow(sum_c2, 2) / n));
+            if (denominator == 0)
+            {
+                return 0.0;
+            }
+            else
+            {
+                return numerator / denominator;
+            }
+        }
+
+        public double TanimotoSimilarityScore(string category_1, string category_2)
+        {
+            if (!ContainsCategory(category_1) || !ContainsCategory(category_2))
+            {
+                return 0.0;
+            }
+
+            Dictionary<string, double>.KeyCollection critic_1_movies = GetFeatureNamesInCategory(category_1);
+            Dictionary<string, double>.KeyCollection critic_2_movies = GetFeatureNamesInCategory(category_2);
+
+            int Na = critic_1_movies.Count;
+            int Nb = critic_2_movies.Count;
+            int Nc = 0;
+
+            foreach (string key1 in critic_1_movies)
+            {
+                foreach (string key2 in critic_2_movies)
+                {
+                    if (key1 == key2)
+                    {
+                        Nc++;
+                    }
+                }
+            }
+
+            // Calculate TanimotoScore
+            double numerator = Nc;
+            double denominator = Na + Nb - Nc;
+            if (denominator == 0)
+            {
+                return 0.0;
+            }
+            else
+            {
+                return numerator / denominator;
+            }
+        }
+
+        #endregion
+
+        #region "Categorisation functions"
+
+        // Recommend n similar categories.
+        public List<CategoryScore> TopNCategoryRecommendations(string category_name, int n, SimilarityScore ScoringFunction)
+        {
+            List<CategoryScore> criticScores = new List<CategoryScore>();
+            foreach (string key in GetCategoryNames())
+            {
+                if (!key.Equals(category_name))
+                {
+                    criticScores.Add(new CategoryScore()
+                    {
+                        name = key,
+                        value = ScoringFunction(category_name, key)
+                    });
+                }
+            }
+
+            criticScores.Sort((x, y) => x.value.CompareTo(y.value));
+            criticScores.Reverse();
+
+            if (n < criticScores.Count)
+            {
+                return criticScores.GetRange(0, n);
+            }
+            else
+            {
+                return criticScores;
+            }
+        }
+
+        // Recommend n features not in category
+        public List<FeatureScore> TopNFeatureRecommendations(string category_name, int n, SimilarityScore ScoringFunction)
+        {
+            List<FeatureScore> rankings = new();
+            Dictionary<string, double> totals = new();
+            Dictionary<string, double> sim_sums = new();
+
+            Dictionary<string, double>.KeyCollection features = GetFeatureNamesInCategory(category_name);
+            double sim_score = 0.0;
+
+            foreach (string key in GetCategoryNames())
+            {
+                if (!key.Equals(category_name))
+                {
+                    sim_score = ScoringFunction(category_name, key);
+                    if (sim_score <= 0.0)   // ignore similarity scores of 0 or lower
+                    {
+                        continue;
+                    }
+
+                    foreach (string candidate in GetFeatureNamesInCategory(key))
+                    {
+                        if (!features.Contains(candidate))
+                        {
+                            double value_for_feature = GetValueForFeatureInCategory(key, candidate);
+                            if (!totals.ContainsKey(candidate))
+                            {
+                                totals[candidate] = (value_for_feature * sim_score);
+                            }
+                            else
+                            {
+                                totals[candidate] += (value_for_feature * sim_score);
+                            }
+                            if (!sim_sums.ContainsKey(candidate))
+                            {
+                                sim_sums[candidate] = sim_score;
+                            }
+                            else
+                            {
+                                sim_sums[candidate] += sim_score;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (string fName in totals.Keys)
+            {
+                rankings.Add(new FeatureScore()
+                {
+                    name = fName,
+                    value = totals[fName] / sim_sums[fName]
+                });
+            }
+
+            rankings.Sort((x, y) => x.value.CompareTo(y.value));
+            rankings.Reverse();
+
+            if (n < rankings.Count)
+            {
+                return rankings.GetRange(0, n);
+            }
+            else
+            {
+                return rankings;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CollaborativeFilteringConsoleTest/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using CollaborativeFiltering;
+using static System.Math;
+
+static void addTestData(Recommendations data)
+{
+    data.AddCategory("Lisa Rose");
+    data.AddCategory("Gene Seymour");
+    data.AddCategory("Michael Phillips");
+    data.AddCategory("Claudia Puig");
+    data.AddCategory("Mick LaSalle");
+    data.AddCategory("Jack Matthews");
+    data.AddCategory("Toby");
+
+    data.AddValueForFeatureToCategory("Lisa Rose", "Lady in the Water", 2.5);
+    data.AddValueForFeatureToCategory("Lisa Rose", "Snakes on a Plane", 3.5);
+    data.AddValueForFeatureToCategory("Lisa Rose", "Just My Luck", 3.0);
+    data.AddValueForFeatureToCategory("Lisa Rose", "Superman Returns", 3.5);
+    data.AddValueForFeatureToCategory("Lisa Rose", "You, Me and Dupree", 2.5);
+    data.AddValueForFeatureToCategory("Lisa Rose", "The Night Listener", 3.0);
+
+    data.AddValueForFeatureToCategory("Gene Seymour", "Lady in the Water", 3.0);
+    data.AddValueForFeatureToCategory("Gene Seymour", "Snakes on a Plane", 3.5);
+    data.AddValueForFeatureToCategory("Gene Seymour", "Just My Luck", 1.5);
+    data.AddValueForFeatureToCategory("Gene Seymour", "Superman Returns", 5.0);
+    data.AddValueForFeatureToCategory("Gene Seymour", "You, Me and Dupree", 3.5);
+    data.AddValueForFeatureToCategory("Gene Seymour", "The Night Listener", 3.0);
+
+    data.AddValueForFeatureToCategory("Michael Phillips", "Lady in the Water", 2.5);
+    data.AddValueForFeatureToCategory("Michael Phillips", "Snakes on a Plane", 3.0);
+    data.AddValueForFeatureToCategory("Michael Phillips", "Superman Returns", 3.5);
+    data.AddValueForFeatureToCategory("Michael Phillips", "The Night Listener", 4.0);
+
+    data.AddValueForFeatureToCategory("Claudia Puig", "Snakes on a Plane", 3.5);
+    data.AddValueForFeatureToCategory("Claudia Puig", "Just My Luck", 3.0);
+    data.AddValueForFeatureToCategory("Claudia Puig", "Superman Returns", 4.0);
+    data.AddValueForFeatureToCategory("Claudia Puig", "You, Me and Dupree", 2.5);
+    data.AddValueForFeatureToCategory("Claudia Puig", "The Night Listener", 4.5);
+
+    data.AddValueForFeatureToCategory("Mick LaSalle", "Lady in the Water", 3.0);
+    data.AddValueForFeatureToCategory("Mick LaSalle", "Snakes on a Plane", 4.0);
+    data.AddValueForFeatureToCategory("Mick LaSalle", "Just My Luck", 2.0);
+    data.AddValueForFeatureToCategory("Mick LaSalle", "Superman Returns", 3.0);
+    data.AddValueForFeatureToCategory("Mick LaSalle", "You, Me and Dupree", 2.0);
+    data.AddValueForFeatureToCategory("Mick LaSalle", "The Night Listener", 3.0);
+
+    data.AddValueForFeatureToCategory("Jack Matthews", "Lady in the Water", 3.0);
+    data.AddValueForFeatureToCategory("Jack Matthews", "Snakes on a Plane", 4.0);
+    data.AddValueForFeatureToCategory("Jack Matthews", "Superman Returns", 5.0);
+    data.AddValueForFeatureToCategory("Jack Matthews", "You, Me and Dupree", 3.5);
+    data.AddValueForFeatureToCategory("Jack Matthews", "The Night Listener", 3.0);
+
+    data.AddValueForFeatureToCategory("Toby", "Snakes on a Plane", 4.5);
+    data.AddValueForFeatureToCategory("Toby", "Superman Returns", 4.0);
+    data.AddValueForFeatureToCategory("Toby", "You, Me and Dupree", 1.0);
+}
+
+static void testCategoryRecommendation(Recommendations data, string category, SimilarityScore scoringFunction, string header)
+{
+    List<CategoryScore> matches = data.TopNCategoryRecommendations(category, 3, scoringFunction);
+
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine(header);
+
+    foreach (CategoryScore match in matches)
+    {
+        Console.WriteLine(match.name + " " + match.value);
+    }
+}
+
+static void testFeatureRecommendation(Recommendations data, string category, SimilarityScore scoringFunction, string header)
+{
+    List<CategoryScore> matches = data.TopNCategoryRecommendations(category, 3, scoringFunction);
+
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+    Console.WriteLine(header);
+
+    foreach (FeatureScore score in data.TopNFeatureRecommendations("Toby", 3, scoringFunction))
+    {
+        Console.WriteLine(score.name + " " + score.value);
+    }
+}
+
+Recommendations data = new();
+addTestData(data);
+
+SimilarityScore scoringFunction = new SimilarityScore(data.PearsonCorrelationScore);
+SimilarityScore scoringFunction2 = new SimilarityScore(data.EuclideanDistanceScore);
+SimilarityScore scoringFunction3 = new SimilarityScore(data.TanimotoSimilarityScore);
+
+testCategoryRecommendation(data, "Toby", scoringFunction, "==== Top 3 Pearson Category Matches ====");
+testCategoryRecommendation(data, "Toby", scoringFunction2, "==== Top 3 Category Euclidean Matches ====");
+testCategoryRecommendation(data, "Toby", scoringFunction3, "==== Top 3 Category Tanimoto Matches ====");
+
+testFeatureRecommendation(data, "Toby", scoringFunction, "==== Top 3 Feature Pearson Matches ====");
+testFeatureRecommendation(data, "Toby", scoringFunction2, "==== Top 3 Feature Euclidean Matches ====");
+testFeatureRecommendation(data, "Toby", scoringFunction3, "==== Top 3 Feature Tanimoto Matches ====");
+
